beb6dd4d1e0db2c67701f338d2ded3c08c1e3e6c
Mitchell Austin <mr.fye@oneandthesame.net>
2021-09-27 11:58:54 -0700

    Improve reuse of styles in NumberControl

--- packages/components/src/number-control/styles/number-control-styles.js
+++ packages/components/src/number-control/styles/number-control-styles.js
@@ -9,50 +9,42 @@ import styled from '@emotion/styled';
  */
 import { InputControlElement } from '../../input-control';
 
-// TODO seems that htmlArrowStyles and defaultArrowStyles should be combined
-const htmlArrowStyles = ( { hideHTMLArrows } ) => {
-	if ( ! hideHTMLArrows ) return ``;
-
-	return css`
-		input[type='number']::-webkit-outer-spin-button,
-		input[type='number']::-webkit-inner-spin-button {
-			-webkit-appearance: none !important;
-			margin: 0 !important;
-		}
-
-		input[type='number'] {
-			-moz-appearance: textfield;
-		}
-	`;
-};
-
-const dragStyles = ( { isDragging, dragCursor } ) => {
-	let defaultArrowStyles;
-	let activeDragCursorStyles;
-
-	if ( isDragging ) {
-		defaultArrowStyles = css`
-			cursor: ${ dragCursor };
-			user-select: none;
-
-			input[type='number']::-webkit-outer-spin-button,
-			input[type='number']::-webkit-inner-spin-button {
-				-webkit-appearance: none !important;
-				margin: 0 !important;
-			}
-		`;
+const styleHiddenHTMLArrows = css`
+	input[type='number']::-webkit-outer-spin-button,
+	input[type='number']::-webkit-inner-spin-button {
+		-webkit-appearance: none !important;
+		margin: 0 !important;
 	}
 
+	input[type='number'] {
+		-moz-appearance: textfield;
+	}
+`;
+
+const htmlArrowStyles = ( { hideHTMLArrows } ) =>
+	hideHTMLArrows ? styleHiddenHTMLArrows : '';
+
+const dragStyles = ( { isDragging, dragCursor, hideHTMLArrows } ) => {
+	let activeDragArrowStyles;
+	let activeDragCursorStyles;
+
+	// While dragging, hide spin arrows if not hidden by hideHTMLArrows prop.
+	if ( ! hideHTMLArrows && isDragging ) {
+		activeDragArrowStyles = styleHiddenHTMLArrows;
+	}
+
+	// While dragging, if a drag cursor is specified style the cursor to it.
 	if ( isDragging && dragCursor ) {
 		activeDragCursorStyles = css`
 			input[type='number']:active {
 				cursor: ${ dragCursor };
+				user-select: none;
 			}
 		`;
 	}
 
 	return css`
-		${ defaultArrowStyles }
+		${ activeDragArrowStyles }
 		${ activeDragCursorStyles }
 	`;
 };

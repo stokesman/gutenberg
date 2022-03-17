9e5f5f02a652db844d0f674c2c64e9d31b2019e7
Mitchell Austin <mr.fye@oneandthesame.net>
2022-03-16 23:06:32 -0700

    Avoid setting unit from effect hook if parsed to `undefined`

--- packages/components/src/unit-control/index.tsx
+++ packages/components/src/unit-control/index.tsx
@@ -79,7 +79,9 @@ function UnforwardedUnitControl(
 	);
 
 	useEffect( () => {
-		setUnit( parsedUnit );
+		if ( parsedUnit !== undefined ) {
+			setUnit( parsedUnit );
+		}
 	}, [ parsedUnit ] );
 
 	// Stores parsed value for hand-off in state reducer.

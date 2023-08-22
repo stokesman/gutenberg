6725bd3280990d0fe501b7424adc0384c8aeee46
Mitchell Austin <mr.fye@oneandthesame.net>
2023-08-22 16:50:00 -0700

    Add test attempting to reproduce

--- test/e2e/specs/editor/various/shortcut-help.spec.js
+++ test/e2e/specs/editor/various/shortcut-help.spec.js
@@ -39,4 +39,36 @@ test.describe( 'keyboard shortcut help modal', () => {
 		await pageUtils.pressKeys( 'access+h' );
 		await expect( dialog ).toBeHidden();
 	} );
+
+	const iterations = 1000;
+	const secondsPerIteration = 0.025
+	test.only( 'doesn’t bug out', async ( {
+		page,
+		pageUtils,
+	} ) => {
+		test.setTimeout( 1000 * 60 * secondsPerIteration * iterations )
+		const wpWrap = page.locator( '#wpwrap' );
+		const editorTopBar = page.locator( 'role=region[name="Editor top bar"]' );
+		const optionsButton = editorTopBar.locator( '[aria-label="Options"]' );
+
+		const menuItem = page.locator( 'role=menuitem', {
+			hasText: /^Keyboard shortcuts/i,
+		} );
+		const dialog = page.locator( 'role=dialog[name="Keyboard shortcuts"]' );
+
+		for ( let i = 0; i < iterations; i++ ) {
+			await optionsButton.click();
+			await menuItem.click();
+			await expect( dialog ).toBeVisible();
+			await expect( wpWrap ).toHaveAttribute( 'inert', '' );
+
+			await page.locator( 'role=button[name="Close"]' ).click();
+			await expect( dialog ).toBeHidden();
+			await expect( wpWrap ).not.toHaveAttribute( 'inert', '' );
+			await expect( menuItem ).toBeFocused();
+
+			await editorTopBar.focus();
+			await expect( editorTopBar ).toBeFocused();
+		}
+	} );
 } );

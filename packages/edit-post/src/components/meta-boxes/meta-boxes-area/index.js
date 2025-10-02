33db8ee4016135330ffa5e232d595855f6d9c00a
Mitchell Austin <mr.fye@oneandthesame.net>
2025-10-01 20:00:56 -0700

    Use dashicons in meta box header buttons

--- packages/edit-post/src/components/meta-boxes/meta-boxes-area/index.js
+++ packages/edit-post/src/components/meta-boxes/meta-boxes-area/index.js
@@ -9,7 +9,6 @@ import clsx from 'clsx';
 import { useRef, useEffect } from '@wordpress/element';
 import { Spinner } from '@wordpress/components';
 import { useSelect } from '@wordpress/data';
-import { arrowUp, chevronUp } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -54,13 +53,7 @@ function MetaBoxesArea( { location } ) {
 	} );
 
 	return (
-		<div
-			className={ classes }
-			style={ {
-				'--wp-icons-arrow-up': `path('${ arrowUp.props.children.props.d }')`,
-				'--wp-icons-chevron-up': `path('${ chevronUp.props.children.props.d }')`,
-			} }
-		>
+		<div className={ classes }>
 			{ isSaving && <Spinner /> }
 			<div
 				className="edit-post-meta-boxes-area__container"
